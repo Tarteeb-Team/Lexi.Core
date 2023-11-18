e32e5a261b921e0b25672b46eb5e9e70399509b0
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-18 19:36:07 +0500

    INFRA: Initialize Infrastructura Build Project

---
+++ Lexi.Core.Api.infrastructura.Build/Program.cs
@@ -0,0 +1,79 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Lexi.Core Build Pipline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        },
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "build",
+
+            new Job
+            {
+                RunsOn = BuildMachines.Windows2022,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Checking out"
+                    },
+
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setting up .Net",
+
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "8.x"
+                        }
+                    },
+
+                    new RestoreTask
+                    {
+                        Name = "Restoring"
+                    },
+
+                    new DotNetBuildTask
+                    {
+                        Name = "Building"
+                    },
+
+                    new TestTask
+                    {
+                        Name = "Testing"
+                    }
+                }
+            }
+        }
+    }
+};
+
+var aDotNetClient = new ADotNetClient();
+
+string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
