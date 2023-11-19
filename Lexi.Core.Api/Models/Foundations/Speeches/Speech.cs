1b9ca53a92d941330b5d2609deb81d1538d389f4
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-11-19 14:21:47 +0500

    DATA: Create & Migrate Speech

---
+++ Lexi.Core.Api/Models/Foundations/Speeches/Speech.cs
@@ -0,0 +1,16 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System;
+
+namespace Lexi.Core.Api.Models.Foundations.Speeches
+{
+    public class Speech
+    {
+        public Guid Id { get; set; }
+        public string Sentence { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
