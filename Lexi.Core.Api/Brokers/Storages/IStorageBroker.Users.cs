5c75ef5d82a871769bab9845f25abcdd4990d1c3
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-20 11:19:24 +0500

    BROKERS: Insert User Model

--- Lexi.Core.Api/Brokers/Storages/IStorageBroker.Users.cs
+++ Lexi.Core.Api/Brokers/Storages/IStorageBroker.Users.cs
@@ -5,15 +5,11 @@
 
 using System.Threading.Tasks;
 using Lexi.Core.Api.Models.Foundations.Users;
-using Microsoft.EntityFrameworkCore;
 
 namespace Lexi.Core.Api.Brokers.Storages
 {
-    public partial class StorageBroker
+    public partial interface IStorageBroker
     {
-        public DbSet<User> Users { get; set; }
-        public async ValueTask<User> InsertUserAsync(User user) =>
-            await InsertAsync(user);
-
+        ValueTask<User> InsertUserAsync(User user);
     }
 }
