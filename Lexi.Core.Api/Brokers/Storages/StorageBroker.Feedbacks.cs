87bc2810ede3ac3f675a97fd67ca0eb92f5d0ca9
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-11-20 16:40:09 +0900

    BROKER: Insert Feedback

--- Lexi.Core.Api/Brokers/Storages/StorageBroker.Feedbacks.cs
+++ Lexi.Core.Api/Brokers/Storages/StorageBroker.Feedbacks.cs
@@ -3,6 +3,8 @@
 // Powering True Leadership
 //=================================
 
+using System.Threading.Tasks;
+using Lexi.Core.Api.Models.Foundations.Feedbacks;
 using Lexi.Core.Api.Models.Foundations.Feedbacks;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,5 +13,8 @@ namespace Lexi.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         DbSet<Feedback> Feedbacks { get; set; }
+
+        public async ValueTask<Feedback> InsertFeedbackAsync(Feedback feedback) =>
+            await InsertAsync(feedback);
     }
 }
