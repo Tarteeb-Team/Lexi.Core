0dc52b8499c19aa75e062502f8d68290aaef5f64
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-20 14:29:21 +0500

    BROKERS: Select User By Id

--- Lexi.Core.Api/Brokers/Storages/IStorageBroker.Users.cs
+++ Lexi.Core.Api/Brokers/Storages/IStorageBroker.Users.cs
@@ -3,6 +3,7 @@
 // Powering True Leadership
 //=================================
 
+using System;
 using System.Threading.Tasks;
 using Lexi.Core.Api.Models.Foundations.Users;
 
@@ -12,5 +13,6 @@ namespace Lexi.Core.Api.Brokers.Storages
     {
         ValueTask<User> InsertUserAsync(User user);
         ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> SelectUserById(Guid id);
     }
 }
--- Lexi.Core.Api/Brokers/Storages/StorageBroker.Users.cs
+++ Lexi.Core.Api/Brokers/Storages/StorageBroker.Users.cs
@@ -3,6 +3,7 @@
 // Powering True Leadership
 //=================================
 
+using System;
 using System.Threading.Tasks;
 using Lexi.Core.Api.Models.Foundations.Users;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +13,12 @@ namespace Lexi.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<User> Users { get; set; }
+
         public async ValueTask<User> InsertUserAsync(User user) =>
             await InsertAsync(user);
         public async ValueTask<User> UpdateUserAsync(User user) =>
             await UpdateUserAsync(user);
-
+        public async ValueTask<User> SelectUserById(Guid id) =>
+            await SelectAsync<User>(id);
     }
 }
