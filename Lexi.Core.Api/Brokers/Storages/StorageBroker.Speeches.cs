25c39c6a46cd8d2ae16dee2a5d09c9f7204869c6
Axliddin Xushboqov <136887631+AxliddinDeveloper@users.noreply.github.com>
2023-11-20 14:12:26 +0500

    Update StorageBroker.Speeches.cs

--- Lexi.Core.Api/Brokers/Storages/StorageBroker.Speeches.cs
+++ Lexi.Core.Api/Brokers/Storages/StorageBroker.Speeches.cs
@@ -12,6 +12,7 @@ namespace Lexi.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Speech> Speeches { get; set; }
+        
         public async ValueTask<Speech> InsertSpeechAsync(Speech speech) =>
             await InsertAsync(speech);
     }
