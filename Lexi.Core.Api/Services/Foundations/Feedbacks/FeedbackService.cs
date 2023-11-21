7afdac5a032c1eb058619ac88d0210d72acf8e82
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-21 15:56:30 +0500

    FOUNDATIONS: Add Feedbacks

---
+++ Lexi.Core.Api/Services/Foundations/Feedbacks/FeedbackService.cs
@@ -0,0 +1,29 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using Lexi.Core.Api.Brokers.Loggings;
+using Lexi.Core.Api.Brokers.Storages;
+using Lexi.Core.Api.Models.Foundations.Feedbacks;
+using System.Threading.Tasks;
+
+namespace Lexi.Core.Api.Services.Foundations.Feedbacks
+{
+    public class FeedbackService : IFeedbackService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public FeedbackService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<Feedback> AddFeedbackAsync(Feedback feedback)
+        {
+            return await this.storageBroker.InsertFeedbackAsync(feedback);
+        }
+    }
+}
---
+++ Lexi.Core.Api/Services/Foundations/Feedbacks/IFeedbackService.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using Lexi.Core.Api.Models.Foundations.Feedbacks;
+using System.Threading.Tasks;
+
+namespace Lexi.Core.Api.Services.Foundations.Feedbacks
+{
+    public interface IFeedbackService
+    {
+        ValueTask<Feedback> AddFeedbackAsync(Feedback feedback);
+    }
+}
