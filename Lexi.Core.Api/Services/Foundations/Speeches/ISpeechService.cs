0db628f913bcfb31439dc2b9695bf92a09cf41d4
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-11-21 10:26:40 +0500

    FOUNDATIONS: Add Speeches

---
+++ Lexi.Core.Api/Services/Foundations/Speeches/ISpeechService.cs
@@ -0,0 +1,11 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+namespace Lexi.Core.Api.Services.Foundations.Speeches
+{
+    public interface ISpeechService
+    {
+    }
+}
