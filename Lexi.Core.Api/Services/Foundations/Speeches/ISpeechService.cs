868126c6cdbee51557fa8e3fea99b336d242410b
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-21 10:44:55 +0500

    FOUNDATIONS: Add Speeches

--- Lexi.Core.Api/Services/Foundations/Speeches/ISpeechService.cs
+++ Lexi.Core.Api/Services/Foundations/Speeches/ISpeechService.cs
@@ -3,9 +3,13 @@
 // Powering True Leadership
 //=================================
 
+using System.Threading.Tasks;
+using Lexi.Core.Api.Models.Foundations.Speeches;
+
 namespace Lexi.Core.Api.Services.Foundations.Speeches
 {
     public interface ISpeechService
     {
+        ValueTask<Speech> AddSpechesAsync(Speech speech);
     }
 }
--- Lexi.Core.Api/Services/Foundations/Speeches/SpeechService.cs
+++ Lexi.Core.Api/Services/Foundations/Speeches/SpeechService.cs
@@ -3,9 +3,26 @@
 // Powering True Leadership
 //=================================
 
+using System.Threading.Tasks;
+using Lexi.Core.Api.Brokers.Loggings;
+using Lexi.Core.Api.Brokers.Storages;
+using Lexi.Core.Api.Models.Foundations.Speeches;
+
 namespace Lexi.Core.Api.Services.Foundations.Speeches
 {
     public class SpeechService : ISpeechService
     {
+        private readonly IStorageBroker storageBroker;
+
+        public ValueTask<Speech> AddSpechesAsync(Speech speech)
+        {
+            return this.storageBroker.InsertSpeechAsync(speech);
+        }
+
+        public SpeechService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
     }
 }
