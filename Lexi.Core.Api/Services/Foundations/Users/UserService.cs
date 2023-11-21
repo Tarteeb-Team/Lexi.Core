daf3bc248ecc6a34cf70ef72fa9789b9f6402177
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-11-21 14:27:15 +0900

    FOUNDATIONS: Retrieve User By Id

---
+++ Lexi.Core.Api/Services/Foundations/Users/UserService.cs
@@ -0,0 +1,34 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Lexi.Core.Api.Brokers.Loggings;
+using Lexi.Core.Api.Brokers.Storages;
+using Lexi.Core.Api.Models.Foundations.Users;
+using Microsoft.Extensions.Logging;
+
+namespace Lexi.Core.Api.Services.Foundations.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public UserService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+        public async ValueTask<User> RetrieveUserByIdAsync(Guid userId)
+        {
+            User persistedUser = await this.storageBroker.SelectUserByIdAsync(userId);
+
+            return persistedUser;
+        }
+    }
+}
