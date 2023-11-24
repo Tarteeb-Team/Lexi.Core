e3f563a483c9bfefead3cc72936e52458033f9b9
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-24 19:41:22 +0500

    FOUNDATIONS: ShouldAddSpeechAsync -> FAIL

---
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Speech/SpeechServiceTests.Logic.Add.cs
@@ -0,0 +1,42 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using Moq;
+using Xunit;
+using FluentAssertions;
+using SpeechModel = Lexi.Core.Api.Models.Foundations.Speeches.Speech;
+
+namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Speech
+{
+    public partial class SpeechServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddSpeechAsync()
+        {
+            //given
+            SpeechModel randomSpeech = CreateRandomSpeech();
+            SpeechModel inputSpeech = randomSpeech;
+            SpeechModel storedSpeech = inputSpeech;
+            SpeechModel expectedSpeech = storedSpeech;
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertSpeechAsync(inputSpeech))
+                .ReturnsAsync(storedSpeech);
+
+            //when
+            SpeechModel actualSpeech =
+                await this.speechService.AddSpechesAsync(inputSpeech);
+
+            //then
+            actualSpeech.Should().NotBeEquivalentTo(expectedSpeech);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertSpeechAsync(inputSpeech), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Speech/SpeechServiceTests.cs
@@ -0,0 +1,36 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using Moq;
+using Tynamix.ObjectFiller;
+using Lexi.Core.Api.Brokers.Loggings;
+using Lexi.Core.Api.Brokers.Storages;
+using Lexi.Core.Api.Services.Foundations.Speeches;
+using SpeechModel = Lexi.Core.Api.Models.Foundations.Speeches.Speech;
+
+namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Speech
+{
+    public partial class SpeechServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ISpeechService speechService;
+
+        public SpeechServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.speechService = new SpeechService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static SpeechModel CreateRandomSpeech() =>
+            CreateSpeechFiller().Create();
+
+        private static Filler<SpeechModel> CreateSpeechFiller() =>
+            new Filler<SpeechModel>();
+    }
+}
