a94af67c84ecd4bed52dc897bd9563695ecd4e1b
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-11-24 22:42:41 +0900

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

---
+++ Lexi.Core.Api/Services/Foundations/Users/UserService.Exceptions.cs
@@ -0,0 +1,37 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System.Threading.Tasks;
+using Lexi.Core.Api.Models.Foundations.Users;
+using Lexi.Core.Api.Models.Foundations.Users.Exceptions;
+using Xeptions;
+
+namespace Lexi.Core.Api.Services.Foundations.Users
+{
+    public partial class UserService
+    {
+        private delegate ValueTask<User> ReturningUserFunction();
+        
+        private async ValueTask<User> TryCatch(ReturningUserFunction returningUserFunction)
+        {
+            try
+            {
+                return await returningUserFunction();
+            }
+            catch (NullUserException nullUserException)
+            {
+                throw CreateAndLogValidationException(nullUserException);
+            }
+        }
+
+        private UserValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var userValidationException = new UserValidationException(exception);
+            this.loggingBroker.LogError(userValidationException);
+
+            return userValidationException;
+        }
+    }
+}
---
+++ Lexi.Core.Api/Services/Foundations/Users/UserService.Validations.cs
@@ -0,0 +1,26 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using Lexi.Core.Api.Models.Foundations.Users;
+using Lexi.Core.Api.Models.Foundations.Users.Exceptions;
+
+namespace Lexi.Core.Api.Services.Foundations.Users
+{
+    public partial class UserService
+    {
+        private void ValidateUserOnAdd(User user)
+        {
+            ValidateUserNotNull(user);
+        }
+
+        private static void ValidateUserNotNull(User user)
+        {
+            if (user == null)
+            {
+                throw new NullUserException();
+            }
+        }
+    }
+}
