b2fdb9056fa2c82d2b1da78e150d37afe594ab6d
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-11-27 23:57:50 +0900

    ShouldThrowNotFoundExceptionOnRemoveIfUserNotFoundAndLogItAsync -> FAIL

--- Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.RemoveById.cs
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.RemoveById.cs
@@ -50,5 +50,45 @@ namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Users
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveIfUserNotFoundAndLogItAsync()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            User nullUser = null;
+
+            var notFoundUserException = new NotFoundUserException(inputId);
+
+            var expectedUserValidationException =
+                new UserValidationException(notFoundUserException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>())).ReturnsAsync(nullUser);
+
+            // when
+            ValueTask<User> removeUserTask = this.userService.RemoveUserAsync(inputId);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(removeUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should()
+                .BeEquivalentTo(expectedUserValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedUserValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteUserAsync(It.IsAny<User>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
