6efadbc883de9f9619e66514571ffc0756a04140
99894 <99894@DESKTOP-K351R76>
2023-11-27 14:14:45 +0500

    ShouldThrowValidationExceptionOnRemoveIfFeedbackIsInvalidAndLogItAsync -> FAIL

--- Lexi.Core.Api.Tests.Unit/Services/Foundations/Feedbacks/FeedbackServiceTests.Validation.Remove.cs
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Feedbacks/FeedbackServiceTests.Validation.Remove.cs
@@ -3,6 +3,7 @@
 // Powering True Leadership
 //=================================
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Lexi.Core.Api.Models.Foundations.Feedbacks;
@@ -37,6 +38,49 @@ namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Feedbacks
             //then
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(feedbackValidationException))),
+                Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteFeedbackAsync(It.IsAny<Feedback>()),
+                Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(default)]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfFeedbackIsInvalidAndLogItAsync(Guid id)
+        {
+            //given
+            Feedback invalidFeedback = new Feedback
+            {
+                Id = id
+            };
+
+            var invalidFeedbackException = new InvalidFeedbackException();
+
+            invalidFeedbackException.AddData(
+                key: nameof(Feedback.Id),
+                values:"Id id required");
+
+            var expectedFeedbackValidationException =
+                new FeedbackValidationException(invalidFeedbackException);
+
+            //when
+            ValueTask<Feedback> feedbackRemoveTask =
+                this.feedbackService.RemoveFeedbackAsync(invalidFeedback);
+
+            FeedbackValidationException feedbackValidationException =
+                await Assert.ThrowsAsync<FeedbackValidationException>(() =>
+                    feedbackRemoveTask.AsTask());
+
+            feedbackValidationException.Should().BeEquivalentTo(expectedFeedbackValidationException);
+
+            //then
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedFeedbackValidationException))),
                 Times.Once());
 
             this.storageBrokerMock.Verify(broker =>
