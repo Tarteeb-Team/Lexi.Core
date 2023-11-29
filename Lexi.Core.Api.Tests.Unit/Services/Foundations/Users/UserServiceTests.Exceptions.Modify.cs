76326c534f4611b390e84610950569b0e948cec8
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-11-29 16:34:02 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Lexi.Core.Api.Models.Foundations.Users;
+using Lexi.Core.Api.Models.Foundations.Users.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            User randomUser = CreateRandomUser();
+            User someUser = randomUser;
+            Guid UserId = someUser.Id;
+            SqlException sqlException = CreateSqlException();
+
+            var failedUserStorageException =
+                new FailedUserStorageException(sqlException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(failedUserStorageException);
+
+            //when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(someUser);
+
+            UserDependencyException actualUserDependencyException =
+                await Assert.ThrowsAsync<UserDependencyException>(
+                    modifyUserTask.AsTask);
+
+            //then
+            actualUserDependencyException.Should().BeEquivalentTo(
+                    expectedUserDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(UserId), Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserAsync(someUser), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
