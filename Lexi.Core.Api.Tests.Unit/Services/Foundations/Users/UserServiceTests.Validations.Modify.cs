f20c5a73d01cff7eb8796b4539a5fc3d6a30e294
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-12-02 10:11:25 +0500

    ShouldThrowValidationExceptionOnModifyIfUserDoesNotExistAndLogItAsync -> FAIL

--- Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Modify.cs
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Modify.cs
@@ -1,4 +1,9 @@
-﻿using FluentAssertions;
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using FluentAssertions;
 using Lexi.Core.Api.Models.Foundations.Users;
 using Lexi.Core.Api.Models.Foundations.Users.Exceptions;
 using Moq;
@@ -92,6 +97,48 @@ namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Users
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUserDoesNotExistAndLogItAsync()
+        {
+            // given
+            User randomUser = CreateRandomUser();
+            User nonExistUser = randomUser;
+            User nullUser = null;
+
+            var notFoundUserException =
+                new NotFoundUserException(nonExistUser.Id);
+
+            var expectedUserValidationException =
+                new UserValidationException(notFoundUserException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(nonExistUser.Id))
+                    .ReturnsAsync(nullUser);
+
+            // when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(nonExistUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>
+                    (modifyUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(expectedUserValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(nonExistUser.Id), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserAsync(nonExistUser), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
