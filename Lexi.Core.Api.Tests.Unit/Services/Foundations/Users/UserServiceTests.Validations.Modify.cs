2f9b7995253999eec9f20fd5866748c95cbbd505
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-12-02 10:04:21 +0500

    ShouldThrowValidationExceptionOnModifyIfUserIsInvalidAndLogItAsync -> FAIL

--- Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Modify.cs
+++ Lexi.Core.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Modify.cs
@@ -45,5 +45,53 @@ namespace Lexi.Core.Api.Tests.Unit.Services.Foundations.Users
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUserIsInvalidAndLogItAsync(string invalidString)
+        {
+            //given
+            User invalidUser = new User
+            {
+                Name = invalidString
+            };
+
+            var invalidUserException =
+                new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Name),
+                values: "Text is required");
+
+            var expectedUserValidationException =
+                new UserValidationException(invalidUserException);
+
+            // when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(invalidUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                    modifyUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should()
+                .BeEquivalentTo(expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserAsync(It.IsAny<User>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
